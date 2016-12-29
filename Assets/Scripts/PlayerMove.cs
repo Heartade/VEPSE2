f4b7fbbb194010cb3ce9ec9343916cfaa8627f28
LeeJS00 <jae2seung@naver.com>
2016-12-29 17:23:08 +0900

    moving
    움직

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour
+{
+  // Use this for initialization
+  public Rigidbody rb;
+  public float vel;
+  void Start()
+  {
+    rb = GetComponent<Rigidbody>();
+  }
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetMouseButton(0))
+    {
+      transform.Translate(Vector3.forward * vel);
+    }
+    if (transform.position.y > 2) transform.position += new Vector3(0, -0.3f, 0);
+  }
+}
